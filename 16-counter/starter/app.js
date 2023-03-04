75ff29fc02bcf2dc61f18771483297216338c48c
Mia Elena <93685530+miaskyelena@users.noreply.github.com>
2023-03-04 17:27:30 -0500

    ok

---
+++ 16-counter/starter/app.js
@@ -0,0 +1,12 @@
+function Counter (element,value){
+    console.log(element,value);
+}
+
+const firstCounter = new Counter()
+
+function getElement (selection){
+    const element = document.querySelector(selection); 
+    console.log(element);
+}
+
+getElement('.first-counter'); 
